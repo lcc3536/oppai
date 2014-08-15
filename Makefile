a9ccb7f1f2fda01deee79b6bc793005812ccb483
linchenchen <linchenchen@leasuregame.com>
2014-08-15 16:54:06 +0800

    add shell

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
-init:
+init: init-submodule init-client
+
+
+init-submodule:
 	git submodule update --init --recursive
+
+init-client:
+	cd client && sh ./bin/init.sh
---
+++ client/bin/init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "============================================================================"
+echo "oppai project client init"
+echo "============================================================================"
+
+echo "1 download package"
+pwd
+#./cocos2d-js/frameworks/js-bindings/cocos2d-x/download-deps.py
+#./cocos2d-js/tools/cocos2d-console/download-bin.py
+
+echo "============================================================================"
+echo "done!"
